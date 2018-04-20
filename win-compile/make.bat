a8b3ecd68725009d1d4d5b0770a3452d90e3e167
Petr Eremeykin <eremeykin@gmail.com>
2018-04-20 17:09:54 +0300

    add win compile folder

---
+++ win-compile/make.bat
@@ -0,0 +1,14 @@
+chdir ..
+IF NOT EXIST .\..\ect GOTO ERROR
+pyinstaller --noconsole --path=.\..\ect --hidden-import sklearn.neighbors.typedefs ect.py
+"C:\Users\Dev\AppData\Local\Programs\Python\Python36\Lib\site-packages\PyQt5\Qt\bin"
+copy "C:\Users\Dev\AppData\Local\Programs\Python\Python36\Lib\site-packages\PyQt5\Qt\bin\*" ".\dist\ect\"
+xcopy ".\ui" ".\dist\ect\ui\*" /s /e
+copy ".\res.qrc" ".\dist\ect\*"
+copy ".\res_rc.py" ".\dist\ect\*"
+copy ".\win-compile\matplotlibrc" ".\dist\ect\mpl-data\" /Y
+rename ".\dist\ect\ect.exe" "INDACT.exe"
+rename ".\dist\ect" "INDACT"
+exit
+:ERROR
+echo "you forgot to copy ect module with clustering libs"
