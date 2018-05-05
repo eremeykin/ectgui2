c36ac235bbd23ea2a3918bd05a30a55369a068ec
Petr Eremeykin <eremeykin@gmail.com>
2018-05-05 10:00:57 +0300

    improve bounds for plot

--- plot/plot.py
+++ plot/plot.py
@@ -5,7 +5,6 @@ from itertools import cycle
 from numpy.linalg import svd
 
 
-
 def plot(ax, data, labels=None, title="Unknown file"):
     colors = cycle('brmyk')
     markers = cycle('ospP*h')
@@ -17,8 +16,10 @@ def plot(ax, data, labels=None, title="Unknown file"):
     for label in ulabels:
         cols[label] = next(colors)
         marks[label] = next(markers)
-    maxx, minx = max(data[:, 0]) * 1.25, min(data[:, 0]) * 1.25
-    maxy, miny = max(data[:, 1]) * 1.25, min(data[:, 1]) * 1.25
+    maxx, minx = max(data[:, 0]), min(data[:, 0])
+    range_x = maxx - minx
+    maxy, miny = max(data[:, 1]), min(data[:, 1])
+    range_y = maxy - miny
     # fig, ax = plt.subplots()
     for label in np.unique(labels):
         ax.scatter(data[labels == label][:, 0], data[labels == label][:, 1],
@@ -26,11 +27,11 @@ def plot(ax, data, labels=None, title="Unknown file"):
                    color=cols[label])
         # ax.annotate(str(labels[p]), (data[p, 0], data[p, 1]))
 
-    ax.plot([minx, maxx], [0, 0], color="black", linewidth=0.5)
-    ax.plot([0, 0], [miny, maxy], color="black", linewidth=0.5)
+    # ax.plot([minx, maxx], [0, 0], color="black", linewidth=0.5)
+    # ax.plot([0, 0], [miny, maxy], color="black", linewidth=0.5)
     ax.set_title(title)
-    ax.set_xlim(minx, maxx)
-    ax.set_ylim(miny, maxy)
+    ax.set_xlim(minx - 0.2 * range_x, maxx + 0.2 * range_x)
+    ax.set_ylim(miny - 0.2 * range_y, maxy + 0.2 * range_y)
     ax.grid(linestyle='dotted')
     # ax.set_aspect('equal', adjustable='box')
 
